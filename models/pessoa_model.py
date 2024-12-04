341eeceb004e4c1a47a6d84ded6f2cb764a8a03f
Pedro Domingos Napole Certo <58532922+pedrocerto1306@users.noreply.github.com>
2024-12-04 20:51:34 -0300

    :construction: - Create pessoa_model.py

---
+++ models/pessoa_model.py
@@ -0,0 +1,15 @@
+import datetime
+from models.model_base import Model_Base
+
+class PessoaModel(Model_Base):
+  def __init__(self, nome: str, cpf: str, plano_id: str, dt_nasc: datetime, altura: float, peso: float):
+    super().__init__(int(cpf.replace(".","").replace("-","")))
+    self.nome = nome
+    self.cpf = cpf
+    self.plano_id = plano_id
+    self.dt_nasc = dt_nasc
+    self.altura = altura
+    self.peso = peso
+
+  def getName(self):
+    print(self.nome)
